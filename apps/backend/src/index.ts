f3ac80275752507215016d62110c45d9e5abd6f6
robsball <158167909+robsball@users.noreply.github.com>
2024-12-08 11:40:07 +0000

    Add root route and bind to Heroku port

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 
 const app = express();
 
+// Example root route
 app.get("/", (req, res) => {
   res.send("Backend is working!");
 });
 
+// Use the Heroku-assigned port or default to 3000 for local development
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
