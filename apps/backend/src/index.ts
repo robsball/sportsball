f2df0f72f1bb50abce9ea0133f58c04ba3a208ad
robsball <158167909+robsball@users.noreply.github.com>
2024-12-08 12:13:27 +0000

    Ensure app binds to Heroku PORT and 0.0.0.0

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 });
 
 // Use the Heroku-assigned port or default to 3000 for local development
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 4000; // Default to 4000 for local dev
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
